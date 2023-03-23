e09da0199d7808bdf36b8996aee2c3b3a8f94921
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-03-23 20:44:43 +0100

    nvim: lua: user: config.lua: Open the file at the same line it was closed at

--- nvim/lua/user/config.lua
+++ nvim/lua/user/config.lua
@@ -25,3 +25,13 @@ if vim.fn.has('termguicolors') == 1 then
 end
 
 vim.cmd('colorscheme peaksea')
+
+-- Open at the same line number it was closed at
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = {"*"},
+    callback = function()
+        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.api.nvim_exec("normal! g'\"",false)
+        end
+    end
+})
