292c73c80f713a72e5dabd12925195e918ee5937
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-05-15 14:33:46 +0200

    init.lua: Suppress warning message

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,12 @@ require "user.config"
 require "user.copilot"
 require "user.grammarous"
 require "user.lsp"
+
+local notify = vim.notify
+vim.notify = function(msg, ...)
+    if msg:match("warning: multiple different client offset_encodings") then
+        return
+    end
+
+    notify(msg, ...)
+end
