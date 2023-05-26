98ea20250a098c39e29910a2535d2e6f29a6926d
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-05-26 12:59:44 +0200

    git: Add pre-commit hook

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 # This script is used to link the dotfiles to the proper
 # locations in the home directory.
 
+set -ex
+
 # Link the nvim config
 ln -sf $(realpath ./nvim/) ~/.config/
 
@@ -12,3 +14,9 @@ ln -sf $(realpath ./git/.gitconfig) ~/.gitconfig
 
 # Link the .tmux.conf
 ln -sf $(realpath ./tmux/.tmux.conf) ~/.tmux.conf
+
+# Add the git hooks
+if [ ! -d "/home/illia/gitconfig/hooks" ]; then
+    mkdir -p /home/illia/gitconfig/hooks
+fi
+ln -sf $(realpath ./git/hooks/pre-commit) /home/illia/gitconfig/hooks/pre-commit
