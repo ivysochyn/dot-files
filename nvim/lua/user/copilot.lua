8c5aa3fc6476c01c22206166bcff37a90dbb900e
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-07-28 16:33:31 +0200

    lua: user: copilot.lua: Toggle copilot by default

--- nvim/lua/user/copilot.lua
+++ nvim/lua/user/copilot.lua
@@ -6,8 +6,9 @@ vim.g.copilot_filetypes = {
 }
 
 -- Map <C-\> to toggle copilot
-vim.g.copilot_enabled = true
-vim.api.nvim_set_keymap('n', '<C-\\>', ':let g:copilot_enabled = !g:copilot_enabled<CR>', { noremap = true, silent = false })
+vim.g.copilot_enabled = false
+-- Set <C-\> to toggle g:copilot_enabled variable and send message to status line
+vim.keymap.set({'i', 'n'}, '<C-\\>', '<Cmd>lua vim.g.copilot_enabled = not vim.g.copilot_enabled<CR><Cmd>echo "Toggled"<CR>', { noremap = true, silent = true })
 
 -- Map <C-]> to <Plug>(copilot-next) and <C-[> to <Plug>(copilot-prev)
 vim.api.nvim_set_keymap('i', '<C-]>', '<Plug>(copilot-next)', { noremap = true, silent = false })
