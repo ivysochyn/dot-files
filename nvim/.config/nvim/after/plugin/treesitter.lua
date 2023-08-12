aba77649d82467e1c61f94916b4363944e4e9dd5
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-08-13 00:43:52 +0200

    nvim: after: treesitter: Change prefered installer to git

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,3 +1,5 @@
+require("nvim-treesitter.install").prefer_git = true
+
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "bash", "json", "yaml", "cpp", "comment", },
